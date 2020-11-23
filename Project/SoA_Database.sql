f387ca2331e58d70f9cdf24937d8d43b4a840068
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2020-11-23 16:34:56 +0100

    Database basic

---
+++ Project/SoA_Database.sql
@@ -0,0 +1,33 @@
+
+
+CREATE DATABASE sonsofanarchy;
+
+\connect sonsofanarchy;
+
+CREATE TABLE "Characters" (
+
+);
+
+CREATE TABLE "Bikes" (
+
+);
+
+CREATE TABLE "Headquarters" (
+
+);
+
+CREATE TABLE "Business" (
+
+);
+
+CREATE TABLE "Criminal_record" (
+
+);
+
+CREATE TABLE "Payroll" (
+
+);
+
+CREATE TABLE "Bands" (
+
+);
