0effc9429808a3953319ad3977c6ff93b4bcca6d
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2020-11-30 17:38:13 +0100

    Database Tables 2

--- Project/SoA_Database.sql
+++ Project/SoA_Database.sql
@@ -8,6 +8,7 @@ CREATE TABLE "Characters" (
     "Character_ID"   VARCHAR (5)  NOT NULL,
     "First_Name"     VARCHAR (20) NOT NULL,
     "Last_Name"      VARCHAR (20) NOT NULL,
+    "Nickname"       VARCHAR (20) NOT NULL,
     "Sex"            CHAR    (1)  NOT NULL,
     "Birthday"       DATE         NOT NULL,
     "Date_Joined"    DATE,
@@ -27,17 +28,27 @@ CREATE TABLE "Bikes" (
 
 CREATE TABLE "Headquarters" (
     "Headquarter_ID" VARCHAR (5)  NOT NULL,
-
+    "Street"         VARCHAR (25) NOT NULL,
+    "City"           VARCHAR (20) NOT NULL,
+    "Postcode"       VARCHAR (10) NOT NULL,
+    "Country"        VARCHAR (20) NOT NULL,
     Constraint "Headquarter_pkey" Primary Key ("Headquarter_ID")
 );
 
 CREATE TABLE "Business" (
-
-    Constraint "Business_pkey" Primary Key ("")
+    "Business_ID"   VARCHAR (5)  NOT NULL,
+    "Business_Type" VARCHAR (20) NOT NULL,
+    "Date"          DATE         NOT NULL,
+    "Deposit"       NUMERIC      NOT NULL,
+    "Client"        VARCHAR (20) NOT NULL,
+    Constraint "Business_pkey" Primary Key ("Business_ID")
 );
 
 CREATE TABLE "Criminal_Record" (
-
+    "Crime_ID" VARCHAR (5)  NOT NULL,
+    "Crime"    VARCHAR (20) NOT NULL,
+    "Jail"     BOOLEAN      NOT NULL,
+    "Sentence" VARCHAR (20) NOT NULL,
     Constraint "Criminal_Record_pkey" Primary Key ("")
 );
 
