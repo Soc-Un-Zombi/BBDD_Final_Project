08d8040a80ef44b46436061f1683cc525ac22413
Joan Francesc Pedro García <joan.f.pedro@protonmail.com>
2020-12-14 16:11:28 +0100

    Update SoA_Database.sql
    Tot el que hi ha de moment

--- Project/SoA_Database.sql
+++ Project/SoA_Database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE sonsofanarchy;
+--CREATE DATABASE sonsofanarchy;
 
 \connect sonsofanarchy;
 
@@ -13,6 +13,7 @@ CREATE TABLE "Characters" (
     "Date_Joined"    DATE,
     "Bike_ID"        VARCHAR (5)  NOT NULL,
     "Headquarter_ID" VARCHAR (5)  NOT NULL
+    -- TODO: Acabar
 );
 
 CREATE TABLE "Bikes" (
@@ -21,6 +22,7 @@ CREATE TABLE "Bikes" (
     "Brand"         VARCHAR (20) NOT NULL,
     "Model"         VARCHAR (20) NOT NULL,
     "Motor"         VARCHAR (20) NOT NULL
+    -- TODO: Acabar
 );
 
 CREATE TABLE "Headquarters" (
@@ -28,7 +30,8 @@ CREATE TABLE "Headquarters" (
     "Street"         VARCHAR (25) NOT NULL,
     "City"           VARCHAR (20) NOT NULL,
     "Postcode"       VARCHAR (10) NOT NULL,
-    "Country"        VARCHAR (20) NOT NULL
+    "Country"        VARCHAR (20) NOT NULL,
+    "Lider_ID"       VARCHAR (5)  NOT NULL
 );
 
 CREATE TABLE "Business" (
@@ -37,28 +40,41 @@ CREATE TABLE "Business" (
     "Date"          DATE         NOT NULL,
     "Deposit"       NUMERIC      NOT NULL,
     "Client"        VARCHAR (20) NOT NULL
+    -- TODO: Acabar
 );
 
 CREATE TABLE "Criminal_Record" (
-    "Crime_ID" VARCHAR (5)  NOT NULL,
-    "Crime"    VARCHAR (20) NOT NULL,
-    "Jail"     BOOLEAN      NOT NULL,
-    "Sentence" VARCHAR (20) NOT NULL
+    "Crime_ID"     VARCHAR (5)  NOT NULL,
+    "Crime"        VARCHAR (20) NOT NULL,
+    "Jail"         BOOLEAN      NOT NULL,
+    "Jail_Street"  VARCHAR (20),
+    "Jail_City"    VARCHAR (20),
+    "Jail_Country" VARCHAR (20),
+    "Sentence"     VARCHAR (20) NOT NULL,
+    "Jury_Date"    DATE         NOT NULL
+    -- TODO: Acabar
 );
 
 CREATE TABLE "Payroll" (
-    "Payroll_ID" VARCHAR(5) NOT NULL
+    "Payroll_ID"    VARCHAR (5)  NOT NULL,
+    "Payroll"       NUMERIC      NOT NULL,
+    "Extra_Payroll" NUMERIC,
+    "Character_ID"  VARCHAR (5)  NOT NULL,
+    "Pay_Date"      DATE         NOT NULL,
 );
 
 CREATE TABLE "Bands" (
-    "Brand_ID" VARCHAR (5)  NOT NULL
+    "Brand_ID"   VARCHAR (5)  NOT NULL,
+    "Brand_Name" VARCHAR (20) NOT NULL,
+    -- TODO: Acabar
 );
 
 CREATE TABLE "Guns" (
     "Gun_ID"   VARCHAR (5)  NOT NULL,
     "Brand"    VARCHAR (20) NOT NULL,
     "Model"    VARCHAR (20) NOT NULL,
-    "Gun_Type" VARCHAR (1)  NOT NULL
+    "Gun_Type" VARCHAR (1)  NOT NULL,
+    -- TODO: Acabar
 );
 
 -- Assignació de claus primàries i foranes
