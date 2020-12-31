7bd4398ad5a2e7d21552ee88417dd5d923bf3664
Joan Francesc Pedro García <joan.f.pedro@protonmail.com>
2020-12-31 16:22:14 +0100

    Update SoA_Database.sql
    Bands i payroll eliminades, falta acabar de millorar taules

--- Project/SoA_Database.sql
+++ Project/SoA_Database.sql
@@ -3,76 +3,68 @@
 --\connect sonsofanarchy;
 
 -- TAULES
-CREATE TABLE Characters (
-    CharacterID VARCHAR (5)  NOT NULL,
-    FirstName   VARCHAR (20) NOT NULL,
-    LastName    VARCHAR (20) NOT NULL,
-    Nickname    VARCHAR (20) NOT NULL,
-    Sex         CHAR    (1)  NOT NULL,
-    Birthday    DATE         NOT NULL,
-    Age         NUMERIC      NOT NULL,
-    ResidenceID VARCHAR (5)  NOT NULL
+
+CREATE TABLE Members (
+    MemberID  INT GENERATED ALWAYS AS IDENTITY,
+    FirstName VARCHAR (20) NOT NULL,
+    LastName  VARCHAR (20) NOT NULL,
+    Nickname  VARCHAR (20) NOT NULL,
+    Sex       CHAR    (1)  NOT NULL,
+    Birthday  DATE         NOT NULL,
+    Age       INT          NOT NULL,
+    Work      VARCHAR (20) NOT NULL,
+    InBand    BOOLEAN      NOT NULL,
+    HouseID   VARCHAR (5)  NOT NULL
 );
 
 CREATE TABLE Bikes (
-    BikeID       VARCHAR (5)  NOT NULL,
-    LicensePlate VARCHAR (7)  NOT NULL,
+    LicensePlate VARCHAR (10) NOT NULL,
     Brand        VARCHAR (20) NOT NULL,
     Model        VARCHAR (20) NOT NULL,
-    Motor        VARCHAR (20) NOT NULL
-    -- TODO: Acabar
+    Motor        VARCHAR (20) NOT NULL,
+    GarageID     VARCHAR (5)  NOT NULL,
+    MemberID     VARCHAR (5)  NOT NULL
 );
 
 CREATE TABLE Headquarters (
-    HeadquarterID VARCHAR (5)  NOT NULL,
-    Street        VARCHAR (25) NOT NULL,
-    City          VARCHAR (20) NOT NULL,
-    Postcode      VARCHAR (10) NOT NULL,
-    Country       VARCHAR (20) NOT NULL,
-    LiderID       VARCHAR (5)  NOT NULL
+    HeadquarterID INT GENERATED ALWAYS AS IDENTITY,
+    LiderID       VARCHAR (5)  NOT NULL,
+    Foundation    DATE         NOT NULL,
+    AdressID      VARCHAR (5)  NOT NULL
 );
 
 CREATE TABLE Business (
-    BusinessID   VARCHAR (5)  NOT NULL,
-    BusinessType VARCHAR (20) NOT NULL,
-    Date         DATE         NOT NULL,
-    Deposit      NUMERIC      NOT NULL,
-    Client       VARCHAR (20) NOT NULL
-    -- TODO: Acabar
+    BusinessID    INT GENERATED ALWAYS AS IDENTITY,
+    HeadquarterID VARCHAR (5)  NOT NULL,
+    BusinessType  VARCHAR (20) NOT NULL,
+    Date          DATE         NOT NULL,
+    Deposit       NUMERIC      NOT NULL,
+    Client        VARCHAR (20) NOT NULL
+);
+
+CREATE TABLE Workers (
+    BusinessID VARCHAR (5) NOT NULL,
+    EmployerID VARCHAR (5) NOT NULL
 );
 
 CREATE TABLE CriminalRecord (
-    CrimeID     VARCHAR (5)  NOT NULL,
+    CrimeID     INT GENERATED ALWAYS AS IDENTITY,
     Crime       VARCHAR (20) NOT NULL,
-    Sentence    VARCHAR (20) NOT NULL,
-    JuryDate    DATE         NOT NULL
-    -- TODO: Acabar i renovar
-);
-
-CREATE TABLE Payroll (
-    PayrollID    VARCHAR (5)  NOT NULL,
-    Payroll      NUMERIC      NOT NULL,
-    ExtraPayroll NUMERIC,
-    CharacterID  VARCHAR (5)  NOT NULL,
-    PayDate      DATE         NOT NULL
-);
-
-CREATE TABLE Bands (
-    BandID   VARCHAR (5)  NOT NULL,
-    BandName VARCHAR (20) NOT NULL
-    -- TODO: Acabar
+    JuryDate    DATE         NOT NULL,
+    JailIn      DATE         NOT NULL,
+    JailOut     DATE
 );
 
 CREATE TABLE Guns (
-    GunID   VARCHAR (5)  NOT NULL,
-    Brand   VARCHAR (20) NOT NULL,
-    Model   VARCHAR (20) NOT NULL,
-    GunType VARCHAR (1)  NOT NULL
-    -- TODO: Acabar
+    GunID    INT GENERATED ALWAYS AS IDENTITY,
+    Brand    VARCHAR (20) NOT NULL,
+    Model    VARCHAR (20) NOT NULL,
+    GunType  VARCHAR (20) NOT NULL,
+    MemberID INT NOT NULL
 );
 
 CREATE TABLE Address (
-    AddressID  INT          NOT NULL,
+    AddressID  INT GENERATED ALWAYS AS IDENTITY,
     Building   VARCHAR (20) NOT NULL,
     Street     VARCHAR (20) NOT NULL,
     Number     INT          NOT NULL,
@@ -83,19 +75,15 @@ CREATE TABLE Address (
 
 -- CLAUS PRIMARIES
 
-ALTER TABLE Characters     ADD CONSTRAINT CharactersPK     PRIMARY KEY (CharacterID);
+ALTER TABLE Members        ADD CONSTRAINT MembersPK        PRIMARY KEY (MemberID);
 
-ALTER TABLE Bikes          ADD CONSTRAINT BikesPK          PRIMARY KEY (BikeID);
+ALTER TABLE Bikes          ADD CONSTRAINT BikesPK          PRIMARY KEY (LicensePlate);
 
 ALTER TABLE Headquarters   ADD CONSTRAINT HeadquartersPK   PRIMARY KEY (HeadquarterID);
 
 ALTER TABLE Business       ADD CONSTRAINT BusinessPK       PRIMARY KEY (BusinessID);
 
 ALTER TABLE CriminalRecord ADD CONSTRAINT CriminalRecordPK PRIMARY KEY (CrimeID);
-
-ALTER TABLE Payroll        ADD CONSTRAINT PayrollPK        PRIMARY KEY (PayrollID);
-
-ALTER TABLE Bands          ADD CONSTRAINT BandsPK          PRIMARY KEY (BandID);
 
 ALTER TABLE Guns           ADD CONSTRAINT GunsPK           PRIMARY KEY (GunID);
 
